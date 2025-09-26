ad84128ec3eccf08faba2a67283f96533bd736eb
Chetan-Vishwakarma <127210376+Chetan-Vishwakarma@users.noreply.github.com>
2025-09-26 19:11:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import puppeteer from "puppeteer";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json({limit: '50mb'}));
+app.use(cors());
 
 app.get("/get",(req,res)=>{
     res.json({message:[{name:"Hello"}]})
